c90fdf640eb0a83b1c55a92574f8c7ca8aa0b58e
Wilson Hobbs <wilsonhobbs1@gmail.com>
2018-02-08 09:12:00 -0500

    wip

---
+++ packages/platform-express/src/execution/bootstrap.ts
@@ -0,0 +1,7 @@
+export function bootstrap(module: any) {
+    // first resolve child injectors - built in to core
+    // extract all resolved routes into their own array
+    // extract middlewares from their array and resolve them into express
+    // cycle over routes to get methods
+    // register routes with Express
+}
